1d93e50f23ed27319f061f94c9ff61f2d9e26745
发明控 <30631809+bilibilifmk@users.noreply.github.com>
2020-01-12 23:15:09 +0800

    屏幕驱动

---
+++ 屏幕驱动/build/CMakeFiles/fbcp-ili9341.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/fbcp-ili9341/diff.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/diff.cpp.o"
+  "/home/pi/fbcp-ili9341/display.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/display.cpp.o"
+  "/home/pi/fbcp-ili9341/dma.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/dma.cpp.o"
+  "/home/pi/fbcp-ili9341/fbcp-ili9341.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/fbcp-ili9341.cpp.o"
+  "/home/pi/fbcp-ili9341/gpu.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/gpu.cpp.o"
+  "/home/pi/fbcp-ili9341/hx8357d.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/hx8357d.cpp.o"
+  "/home/pi/fbcp-ili9341/ili9341.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/ili9341.cpp.o"
+  "/home/pi/fbcp-ili9341/ili9486.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/ili9486.cpp.o"
+  "/home/pi/fbcp-ili9341/keyboard.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/keyboard.cpp.o"
+  "/home/pi/fbcp-ili9341/low_battery.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/low_battery.cpp.o"
+  "/home/pi/fbcp-ili9341/mailbox.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/mailbox.cpp.o"
+  "/home/pi/fbcp-ili9341/mem_alloc.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/mem_alloc.cpp.o"
+  "/home/pi/fbcp-ili9341/mpi3501.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/mpi3501.cpp.o"
+  "/home/pi/fbcp-ili9341/mz61581.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/mz61581.cpp.o"
+  "/home/pi/fbcp-ili9341/spi.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/spi.cpp.o"
+  "/home/pi/fbcp-ili9341/ssd1351.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/ssd1351.cpp.o"
+  "/home/pi/fbcp-ili9341/st7735r.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/st7735r.cpp.o"
+  "/home/pi/fbcp-ili9341/statistics.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/statistics.cpp.o"
+  "/home/pi/fbcp-ili9341/text.cpp" "/home/pi/fbcp-ili9341/build/CMakeFiles/fbcp-ili9341.dir/text.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/vc/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 屏幕驱动/build/CMakeFiles/fbcp-ili9341.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fbcp-ili9341.dir/diff.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/display.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/dma.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/fbcp-ili9341.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/gpu.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/hx8357d.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/ili9341.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/ili9486.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/keyboard.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/low_battery.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/mailbox.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/mem_alloc.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/mpi3501.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/mz61581.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/spi.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/ssd1351.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/st7735r.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/statistics.cpp.o"
+  "CMakeFiles/fbcp-ili9341.dir/text.cpp.o"
+  "fbcp-ili9341.pdb"
+  "fbcp-ili9341"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fbcp-ili9341.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
